fdcba80de4c75f0ab7dccb4caad7b1c3d9611711
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-25 23:55:06 -0300

    styled-components

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,7 +1,21 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+
+  button {
+    background-color: #0e1d25;
+  }
+`;
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Title className="title">
+        Hello
+        <button>Salvar</button>
+      </Title>
     </div>
   );
 }
