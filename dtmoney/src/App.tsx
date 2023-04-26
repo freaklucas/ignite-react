5b5f7a81a70b74e7f8de6c6c63e413291a8cc819
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-26 00:18:56 -0300

    config css

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,21 +1,10 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-
-  button {
-    background-color: #0e1d25;
-  }
-`;
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <Title className="title">
-        Hello
-        <button>Salvar</button>
-      </Title>
+      <h1>Olá!</h1>
+      <GlobalStyle />
     </div>
   );
 }
