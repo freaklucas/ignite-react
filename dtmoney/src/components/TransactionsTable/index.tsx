90450feff1d21485dde91211a80fcb3831077996
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-26 18:02:36 -0300

    componente de tabela

---
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="widthdraw">-R$ 10.000</td>
+            <td>Desenvolvimento</td>
+            <td>26/04/2023</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="deposit">R$ 1.200</td>
+            <td>Casa</td>
+            <td>16/04/2023</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
