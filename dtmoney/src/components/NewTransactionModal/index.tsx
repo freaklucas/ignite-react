37efc1b13b24a1e2dff95e51cfed264c48f7dcff
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-27 14:52:38 -0300

    content modal

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,36 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTramsactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}:NewTramsactionModalProps) {
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTramsactionModalProps) {
   return (
-    <Modal
-    isOpen={isOpen}
-    onRequestClose={onRequestClose}
-  >
-    <h2>Cadastrar transação</h2>
-  </Modal>
+    <Modal 
+      isOpen={isOpen} 
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar transação</h2>
+        <input 
+          placeholder="Título" 
+        />
+        <input 
+          placeholder="Valor" 
+          type="number" 
+        />
+        <input 
+          placeholder="Categoria" 
+        />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
   );
 }
